0c737596f8dd07bb1123f22a3792ad8c1e168361
WilliamChou123 <wchou28@shschools.org>
2026-05-29 15:18:58 -0700

    fix

--- test.py
+++ test.py
@@ -2,12 +2,16 @@ from nanobtcontrol import IPodNano
 import time
 
 ipod = IPodNano()
-ipod.scan("hi")
-print("found ipod: " + ipod.get_name())
-time.sleep(2)
-ipod.connect()
-while True:
-    ipod.play()
-    time.sleep(1)
-    ipod.pause()
-    time.sleep(1)
+search = input("ipodname")
+ipod.scan(search)
+if ipod.get_mac:
+    print("found ipod: " + ipod.get_name())
+    time.sleep(2)
+    ipod.connect()
+    while True:
+        ipod.play()
+        time.sleep(1)
+        ipod.pause()
+        time.sleep(1)
+else:
+    print("failed to find ipod with name: " + search)
