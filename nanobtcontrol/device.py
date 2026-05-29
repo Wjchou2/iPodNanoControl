73baf721937bbefea102446b2c589680e055201b
WilliamChou123 <wchou28@shschools.org>
2026-05-29 15:31:39 -0700

    check off

--- nanobtcontrol/device.py
+++ nanobtcontrol/device.py
@@ -27,7 +27,7 @@ class IPodNano:
                 parts = line.split(" ", 2)
                 self.mac = parts[1]
                 self.name = parts[2]
-                subprocess.run(["bluetoothctl", "scan", "off"], check=True)
+                subprocess.run(["bluetoothctl", "scan", "off"])
                 return self
 
         return None
