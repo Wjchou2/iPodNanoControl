a0e0b6658147054cefadec3a6db1a454be869acf
WilliamChou123 <wchou28@shschools.org>
2026-05-29 15:34:05 -0700

    hi

--- test.py
+++ test.py
@@ -8,14 +8,13 @@ if ipod.get_mac():
     print(ipod.get_name())
     time.sleep(2)
     ipod.connect()
-    ipod.stop()
-    print("Stopping")
-    time.sleep(2)
+    # ipod.stop()
+    # print("Stopping")
+    # time.sleep(2)
 
     while True:
         ipod.next()
         print(ipod.track())
-        time.sleep(0.1)
 
 else:
     print("failed to find ipod with name: ")
